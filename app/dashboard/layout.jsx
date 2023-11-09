924a733c3500f0a0db05088c987b84f49d1eda5d
macboock <macboock@macboocks-MacBook-Pro.local>
2023-11-09 02:32:23 -0800

    pass the childer and dsiplay in dashboaed

--- app/dashboard/layout.jsx
+++ app/dashboard/layout.jsx
@@ -1,6 +1,6 @@
 import Navbar from "../ui/dashboard/navbar/navbar"
 import Sidebar from "../ui/dashboard/sidebar/sidebar"
-const Layout = () => {
+const Layout = ({ children }) => {
   return (
     <div>
       <div>
