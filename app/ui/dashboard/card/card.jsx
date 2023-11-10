20ba902ec61161e20a1bbec1d55863407f3ad0e9
macboock <macboock@macboocks-MacBook-Pro.local>
2023-11-10 00:19:40 -0800

    Add card details

--- app/ui/dashboard/card/card.jsx
+++ app/ui/dashboard/card/card.jsx
@@ -1,10 +1,18 @@
-import styles from "./card.module.css"
+import styles from "./card.module.css";
+import { MdSupervisedUserCircle } from "react-icons/md";
 const Card = () => {
   return (
-    <div>
-    <div className={styles.cards}>card</div>
+    <div className={styles.container}>
+      <MdSupervisedUserCircle size={24} />
+      <div className={styles.texts}>
+        <span className={styles.title}>Total User</span>
+        <span className={styles.number}>10230</span>
+        <span className={styles.detail}>
+          <span className={styles.positive}>12%</span> More than previous
+        </span>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Card
+export default Card;
