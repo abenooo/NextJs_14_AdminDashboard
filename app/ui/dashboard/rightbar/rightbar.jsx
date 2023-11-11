1f3113f6e8531b46a01bb58f6aaba8406002e28a
macboock <macboock@macboocks-MacBook-Pro.local>
2023-11-11 04:58:55 -0800

    Add right bar structure

--- app/ui/dashboard/rightbar/rightbar.jsx
+++ app/ui/dashboard/rightbar/rightbar.jsx
@@ -1,7 +1,52 @@
+import Image from "next/image";
+import styles from "./rightbar.module.css";
+import { MdReadMore } from "react-icons/md";
 const Rightbar = () => {
   return (
-    <div>rightbar</div>
-  )
-}
+    <div className={styles.container}>
+      <div className={styles.item}>
+        <div className={styles.bgContainer}>
+          <Image src="/astronaut.png" alt="" fill className={styles.bg}/>
+        </div>
+        <div className={styles.texts}>
+          <span className={styles.notification}>Available Now</span>
+          <h3 className={styles.title}>How to use the new version of the admin dashboard?</h3>
+          <span className={styles.subtitle}>Takes 4 minite to learn</span>
+          <p className={styles.desc}>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate
+            non possimus similique quia rerum id aut ad odio, architecto eaque
+            nisi, nostrum quasi asperiores veniam in saepe beatae, placeat
+            ipsum.
+          </p>
+          <button className={styles.button}>
+            <MdReadMore />
+            Learn
+          </button>
+        </div>
+      </div>
+{/* card two */}
+<div className={styles.item}>
+        <div className={styles.bgContainer}>
+          <Image src="/astronaut.png" alt="" fill className={styles.bg}/>
+        </div>
+        <div className={styles.texts}>
+          <span className={styles.notification}>Available Now</span>
+          <h3>How to use the new version of the admin dashboard?</h3>
+          <span className={styles.subtitle}>Takes 4 minite to learn</span>
+          <p className={styles.desc}>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate
+            non possimus similique quia rerum id aut ad odio, architecto eaque
+            nisi, nostrum quasi asperiores veniam in saepe beatae, placeat
+            ipsum.
+          </p>
+          <button className={styles.button}>
+            <MdReadMore />
+            Learn
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default Rightbar
+export default Rightbar;
