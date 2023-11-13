31e1410f00825dbb8dd75a7d930a51786f50e143
macboock <macboock@macboocks-MacBook-Pro.local>
2023-11-13 00:42:27 -0800

    Finsihe the user top side

--- app/dashboard/users/page.jsx
+++ app/dashboard/users/page.jsx
@@ -1,10 +1,14 @@
 import Search from "@/app/ui/dashboard/search.jsx/search";
 import styles from "@/app/ui/user/user.module.css";
+import Link from "next/link";
 const UsersPage = () => {
   return (
     <div className={styles.container}>
       <div className={styles.top}>
         <Search placeholder="Search here for users ..." />
+        <Link href="/dashboard/users/add">
+          <button className={styles.addButton}>Add New</button>
+        </Link>
       </div>
 
       <table className={styles.table}></table>
