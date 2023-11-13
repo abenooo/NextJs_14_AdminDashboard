e93468347cfffeb589c2a6cf55b5954394cbdd5d
macboock <macboock@macboocks-MacBook-Pro.local>
2023-11-13 00:33:33 -0800

    Accept the props and pass the placeholder

--- app/dashboard/users/page.jsx
+++ app/dashboard/users/page.jsx
@@ -1,11 +1,15 @@
-import React from 'react'
-import User from '@/app/ui/user/user'
+import Search from "@/app/ui/dashboard/search.jsx/search";
+import styles from "@/app/ui/user/user.module.css";
 const UsersPage = () => {
   return (
-    <div>UsersPage
-      <User />
-    </div>
-  )
-}
+    <div className={styles.container}>
+      <div className={styles.top}>
+        <Search placeholder="Search here for users ..." />
+      </div>
 
-export default UsersPage
+      <table className={styles.table}></table>
+    </div>
+  );
+};
+
+export default UsersPage;
