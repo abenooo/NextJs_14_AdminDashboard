7a4a8d6d59334f0f50bbeffceb65d68be68e87e0
macboock <macboock@macboocks-MacBook-Pro.local>
2023-11-19 23:25:09 -0800

    Complete UI

--- app/dashboard/products/[id]/page.jsx
+++ app/dashboard/products/[id]/page.jsx
@@ -9,34 +9,34 @@ const SingleUserPage = () => {
         <div className={styles.imgContainer}>
           <Image src="/noavatar.png" alt="" fill />
         </div>
-        John Doe
+        Iphone
       </div>
       {/* right side */}
       <div className={styles.formContainer}>
         <form className={styles.form}>
-          <label>Username</label>
-          <input type="text " name="username" placeholder="John Doe" />
-          <label>Email</label>
-          <input type="text " name="email" placeholder="JohnDoe@admin.com" />
-          <label>Password</label>
-          <input type="text " name="password" placeholder="***********" />
-          <label>Phone</label>
-          <input type="text " name="phone" placeholder="+251-123-456-7890" />
-          <label>Address</label>
-          <textarea type="text " name="address" placeholder="Addis Ababa" />
-          <label>Is Admin</label>
-          <select name="isAdmin" id="isAdmin">
-            <option value={true}>Yes</option>
-            <option value={false}>No</option>
+          <label>Title</label>
+          <input type="text " name="title" placeholder="title" />
+          <label>Price</label>
+          <input type="text " name="price" placeholder="1000$" />
+          <label>Stock</label>
+          <input type="text " name="stock" placeholder="10" />
+          <label>Color</label>
+          <input type="text " name="color" placeholder="red" />
+          <label>Size</label>
+          <textarea type="text " name="size" placeholder="xl" />
+          <label>Category</label>
+          <select name="cat" id="cat">
+            <option value="kitchen">Kitchen</option>
+            <option value="computer">Computer</option>
           </select>
-          <label>Is Active</label>
-          <select name="isActive" id="isActive">
-            <option value={true}>Yes</option>
-            <option value={false}>No</option>
-          </select>
-          <button>
-            Update
-          </button>
+          <label>Description</label>
+          <textarea
+            name="desc"
+            id="desc"
+            rows="10"
+            placeholder="description"
+          ></textarea>
+          <button>Update</button>
         </form>
       </div>
     </div>
