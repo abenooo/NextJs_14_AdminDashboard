7c36cb3140565504d5efcdc2d8a3141ba26332a3
macboock <macboock@macboocks-MacBook-Pro.local>
2023-11-22 00:43:01 -0800

    Add search functionlity

--- app/dashboard/users/page.jsx
+++ app/dashboard/users/page.jsx
@@ -4,12 +4,14 @@ import Image from "next/image";
 import Link from "next/link";
 import Pagination from "@/componet/pagination/pagination";
 import { fetchUsers } from "@/app/lib/data";
-import moment from 'moment';
-const UsersPage = async() => {
-  const users = await fetchUsers();
+import moment from "moment";
+const UsersPage = async ({searchParams}) => {
+
+  const q = searchParams?.q || "";
+  // console.log(q)
+  const users = await fetchUsers(q);
   // console.log(users, "users");
 
-  
   return (
     <div className={styles.container}>
       <div className={styles.top}>
@@ -31,36 +33,38 @@ const UsersPage = async() => {
           </tr>
         </thead>
         <tbody>
-        {users.map(user => (
-          <tr key={user.id}>
-            <td>
-              <div className={styles.user}>
-                <Image
-                  src="/noavatar.png"
-                  alt=""
-                  width={40}
-                  height={40}
-                  className={styles.userImage}
-                />
-                {user.username}
-              </div>
-            </td>
-            <td>{user.email}</td>
-            <td>{moment(user.createdAt).format('MMMM Do YYYY, h:mm:ss a')}</td>
-            <td>{user.isAdmin ? "Admin" : "Client"}</td>
-            <td>{user.isActive ? "Active" : "not Active"}</td>
-            <td>
-              <Link href="/dashboard/users/${user.id}">
-                <button className={`${styles.button} ${styles.view}`}>
-                  View
+          {users.map((user) => (
+            <tr key={user.id}>
+              <td>
+                <div className={styles.user}>
+                  <Image
+                    src="/noavatar.png"
+                    alt=""
+                    width={40}
+                    height={40}
+                    className={styles.userImage}
+                  />
+                  {user.username}
+                </div>
+              </td>
+              <td>{user.email}</td>
+              <td>
+                {moment(user.createdAt).format("MMMM Do YYYY, h:mm:ss a")}
+              </td>
+              <td>{user.isAdmin ? "Admin" : "Client"}</td>
+              <td>{user.isActive ? "Active" : "not Active"}</td>
+              <td>
+                <Link href="/dashboard/users/${user.id}">
+                  <button className={`${styles.button} ${styles.view}`}>
+                    View
+                  </button>
+                </Link>
+                <button className={`${styles.button} ${styles.delete}`}>
+                  Delete
                 </button>
-              </Link>
-              <button className={`${styles.button} ${styles.delete}`}>
-                Delete
-              </button>
-            </td>
-          </tr>
-               ))}
+              </td>
+            </tr>
+          ))}
         </tbody>
       </table>
 
